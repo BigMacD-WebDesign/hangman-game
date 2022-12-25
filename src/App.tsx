69ffa6aea63c6cb05581f7e9c6ee1b9db87b667f
Scott MacDonald <scottmacd88@yahoo.com>
2022-12-24 20:30:12 -0700

    added react and ts skeletons using vite and created list of words to pull hangman game from

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import words from "./wordList.json";
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  });
+
+  console.log(wordToGuess);
+  return (
+   <h1>Hello World</h1>
+  )
+}
+
+export default App
